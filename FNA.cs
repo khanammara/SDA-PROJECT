a7c0a6f0be71dd1f9404604bcef64c339fec5cae
Ammara Riaz <45735724+khanammara@users.noreply.github.com>
2020-06-23 11:12:18 -0700

    FNA.cs

---
+++ FNA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TMS
+{
+    public partial class FNA : MetroFramework.Forms.MetroForm
+    {
+        public FNA()
+        {
+            InitializeComponent();
+        }
+
+        private void FNA_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ManagerDashboard form = new ManagerDashboard();
+            this.Hide();
+            form.FormClosed += new FormClosedEventHandler(delegate { Close(); });
+            form.Show();
+        }
+    }
+}
